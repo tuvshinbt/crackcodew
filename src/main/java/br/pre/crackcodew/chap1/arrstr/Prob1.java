9b4454ddb530b5d04d4da22450c6897797c5941a
Battuvshin Badarch <tuvshin0bt@gmail.com>
2019-02-19 15:29:15 -0600

    Problem1's solution

--- src/main/java/br/pre/crackcodew/chap1/arrstr/Prob1.java
+++ src/main/java/br/pre/crackcodew/chap1/arrstr/Prob1.java
@@ -1,5 +1,43 @@
 package br.pre.crackcodew.chap1.arrstr;
 
+/**
+ * Is Unique: Implement an algorithm to determine if a string has all unique
+ * characters. What if you cannot use additional data structures?
+ * 
+ * @author bbadarch
+ *
+ */
 public class Prob1 {
 
+	public static void main(String[] args) {
+		Prob1 prob1 = new Prob1();
+		System.out.println("Result of isUniqueChars - " + prob1.isUniqueChars("abc"));
+		System.out.println("Result of isUniqueChars2 - " + prob1.isUniqueChars2("dca"));
+	}
+
+	boolean isUniqueChars(String str) {
+		if (str.length() > 128)
+			return false;
+		boolean[] char_set = new boolean[128];
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i);
+			if (char_set[val]) { // Already found this char in string
+				return false;
+			}
+			char_set[val] = true;
+		}
+		return true;
+	}
+
+	boolean isUniqueChars2(String str) {
+		int checker = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i) - 'a';
+			if ((checker & (1 << val)) > 0) {
+				return false;
+			}
+			checker |= (1 << val);
+		}
+		return true;
+	}
 }
---
+++ src/test/java/br/pre/crackcodew/chap1/arrstr/Prob1Test.java
@@ -0,0 +1,7 @@
+package br.pre.crackcodew.chap1.arrstr;
+
+import junit.framework.TestCase;
+
+public class Prob1Test extends TestCase {
+
+}
